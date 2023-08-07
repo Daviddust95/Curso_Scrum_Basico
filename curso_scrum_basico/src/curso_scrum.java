c819941a1fb7829d96876b571a0b2d0dc7930ae8
Alisson David de Melo <daviddust4@hotmail.com>
2023-08-07 20:52:10 -0300

    Add files via upload

---
+++ curso_scrum_basico/src/curso_scrum.java
@@ -0,0 +1,43 @@
+public class curso_scrum {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Seção 2: Scrum - Parte 1
+         * O scrum possui 3 artefatos (product backlog), spring backlog e o increment.
+         * 
+         * 
+         * O scrum possui 4 eventos (Spring planning, Daily Scrum, Spring Review, Spring
+         * Retrospective).
+         * 
+         * 
+         * O scrum possui 5 valores que governam o mindset scrum (coragem, foco,
+         * comprometimento, respeito e
+         * abertura).
+         * 
+         * Coragem: O time Scrum precisa ter coragem para fazer a coisa certa e
+         * trabalhar em programas dificeis.
+         * 
+         * Foco: Todos focam no trabalho da Sprint e nos objetivos do time Scrun.
+         * Comprometimento: As pessoas se comprometem pessoalmente em alcançar os
+         * objetivos do time Scrun.
+         * 
+         * Respeito: Os membros do time Scrun respeitam uns aos outros para serem
+         * pessoas capazes e independentes.
+         * 
+         * Abertura: O time Scrun e seus Stakeholders concordam em estarem abertos a
+         * todo o trabalho e aos desafios com a execução dos trabalhos.
+         * 
+         * 
+         * O scrum possui 3 papéis( Scrum Master, Product Owner, Desenvolvedores).
+         * 
+         *
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         * 
+         */
+    }
+}
